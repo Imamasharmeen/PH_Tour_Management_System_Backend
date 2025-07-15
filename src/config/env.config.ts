3b82e6395de1b764e2d592113f47ac3284b3e90a
Imamasharmeen <imamasharmeen@gmail.com>
2025-07-16 01:07:41 +0300

    Initialize server with MongoDB connection and app listener.

--- src/config/env.config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv  from 'dotenv';
-
-dotenv.config();
-export const env_config={
-    port : process.env.PORT || 5000,
-    mongodb_url : process.env.MONGODB_URL as string,
-    node_env : process.env.NODE_ENV || "development",
-}
